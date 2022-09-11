b127fec2fa7f092e6901ea6a9741d269ff9d6e70
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2022-09-11 21:40:42 +0200

    improve simple holiday

--- src/app/holidays/simple-holiday-card.component.ts
+++ src/app/holidays/simple-holiday-card.component.ts
@@ -1,20 +1,38 @@
-import { Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 import { HolidayCardsComponent } from './holiday-cards-component';
 import { Holiday } from './model/holiday';
-import { CommonModule } from '@angular/common';
+import { NgForOf, NgStyle } from '@angular/common';
 
 @Component({
   template: ` <div
-    *ngFor="let holiday of holidays"
+    *ngFor="let holiday of viewModel"
     class="big-image"
-    [ngStyle]="{ 'background-image': 'url(' + holiday.imageUrl + ')' }"
+    [ngStyle]="holiday.image"
     data-testid="holiday"
   >
     <h2>{{ holiday.title }}</h2>
   </div>`,
   standalone: true,
-  imports: [CommonModule],
+  imports: [NgForOf, NgStyle],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SimpleHolidayCardsComponent implements HolidayCardsComponent {
+export class SimpleHolidayCardsComponent
+  implements HolidayCardsComponent, OnChanges
+{
   @Input() holidays: Holiday[] = [];
+
+  viewModel: { title: string; image: { 'background-image': string } }[] = [];
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.viewModel = this.holidays.map((holiday) => ({
+      title: holiday.title,
+      image: { 'background-image': `url('${holiday.imageUrl}')` },
+    }));
+  }
 }
