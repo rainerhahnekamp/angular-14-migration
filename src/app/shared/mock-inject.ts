83bc2acc9f07bc7eeef6b354b9c88101f463b08c
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2022-09-11 21:01:54 +0200

    add mock-inject

---
+++ src/app/shared/mock-inject.ts
@@ -0,0 +1,47 @@
+import * as angularCore from '@angular/core';
+import { ProviderToken } from '@angular/core';
+
+export class MockInject {
+  mocks = new Map<ProviderToken<unknown>, unknown>();
+  spy: jest.SpyInstance;
+  constructor() {
+    this.spy = jest.spyOn(angularCore, 'inject');
+    this.spy.mockImplementation((token: ProviderToken<unknown>) => {
+      if (this.mocks.has(token)) {
+        return this.mocks.get(token);
+      } else {
+        throw new Error('injector was called with unknown token: ' + token);
+      }
+    });
+  }
+
+  with(token: ProviderToken<unknown>, mock: unknown) {
+    this.mocks.set(token, mock);
+    return this;
+  }
+
+  reset() {
+    this.spy.mockRestore();
+  }
+}
+
+export class SafeMockInject extends MockInject {
+  override with<T>(token: ProviderToken<T>, mock: T) {
+    super.with(token, mock);
+    return this;
+  }
+}
+
+export const mockInject = {
+  with: (token: ProviderToken<unknown>, mock: unknown) => {
+    const instance = new MockInject();
+    return instance.with(token, mock);
+  },
+};
+
+export const safeMockInject = {
+  with: (token: ProviderToken<unknown>, mock: unknown) => {
+    const instance = new SafeMockInject();
+    return instance.with(token, mock);
+  },
+};
